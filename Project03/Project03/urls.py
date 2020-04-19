5e02132ef736000f45dfda86c24286aa336a97bc
Ritesh Patel <pater16@mcmaster.ca>
2020-04-19 20:52:12 +0000

    Changed instances of macid

--- Project03/Project03/urls.py
+++ Project03/Project03/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('e/macid/', include('login.urls')),
-    path('e/macid/social/', include('social.urls')),
+    path('e/pater16/', include('login.urls')),
+    path('e/pater16/social/', include('social.urls')),
 ]
